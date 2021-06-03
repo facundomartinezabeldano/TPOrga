a77d1e32cdcf551c4a453c8686fb93611aaad98b
Facundo <57848306+facundomartinezabeldano@users.noreply.github.com>
2021-06-03 13:56:01 -0300

    Update

--- Taller 4/checkpoint2.asm
+++ Taller 4/checkpoint2.asm
@@ -1,11 +1,11 @@
-JMP seguir
-seguir:
-SET R0, 0xFF
-SET R1, 0x11
+0x00                JMP seguir
+0x02        seguir:
+0x02                SET R0, 0xFF
+0x04                SET R1, 0x11
 
-siguiente:
-ADD R0, R1
-JC siguiente
+0x06        siguiente:
+0x06                ADD R0, R1
+0x08                JC siguiente
 
-halt:
-JMP halt
+0x0A        halt:
+0x0A                JMP halt
